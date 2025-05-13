4e6213818df22ae684b4146dd408f1f51e0427c5
Simon Levesque <simon@foilen.com>
2025-05-13 18:32:39 -0400

    Add ConfigMap listing

--- scripts/k8s_all_get.sh
+++ scripts/k8s_all_get.sh
@@ -3,6 +3,10 @@
 kubectl get all -o wide --all-namespaces | tee _get_all.txt
 
 echo | tee -a _get_all.txt
+echo '== ConfigMap ==' | tee -a _get_all.txt
+kubectl get configmap --all-namespaces | tee -a _get_all.txt
+
+echo | tee -a _get_all.txt
 echo '== Secrets ==' | tee -a _get_all.txt
 kubectl get secret --all-namespaces | tee -a _get_all.txt
 
@@ -13,4 +17,3 @@ kubectl get pvc --all-namespaces | tee -a _get_all.txt
 echo | tee -a _get_all.txt
 echo '==Persistent Volume==' | tee -a _get_all.txt
 kubectl get pv --all-namespaces | tee -a _get_all.txt
-
