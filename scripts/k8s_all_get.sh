de75052cd56484683e0b963796fc21f247ee258a
Simon Levesque <simon@foilen.com>
2025-05-13 17:02:02 -0400

    Add listing of secrets, PVC and PV

--- scripts/k8s_all_get.sh
+++ scripts/k8s_all_get.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
 kubectl get all -o wide --all-namespaces | tee _get_all.txt
+
+echo | tee -a _get_all.txt
+echo '== Secrets ==' | tee -a _get_all.txt
+kubectl get secret --all-namespaces | tee -a _get_all.txt
+
+echo | tee -a _get_all.txt
+echo '==Persistent Volume Claim==' | tee -a _get_all.txt
+kubectl get pvc --all-namespaces | tee -a _get_all.txt
+
+echo | tee -a _get_all.txt
+echo '==Persistent Volume==' | tee -a _get_all.txt
+kubectl get pv --all-namespaces | tee -a _get_all.txt
+
